a9e361dc17cf821183c66b8317177cb93291bc2e
19056685 <19056685@cnsuning.com>
2023-12-26 16:07:30 +0800

    src-DoubleTest

---
+++ src/DoubleTest.java
@@ -0,0 +1,13 @@
+public class DoubleTest {
+
+    public static void main(String[] args) {
+        double d = 1.0;
+        Integer i = null;
+        if (d > i) {
+            System.out.println("what!?");
+        } else {
+            System.out.println("!?");
+        }
+    }
+
+}
